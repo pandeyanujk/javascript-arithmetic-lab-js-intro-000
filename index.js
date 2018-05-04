caaea958d8f8f0306016e4434f55569d5899d06a
Anuj Pandey <pandey.anujk@gmail.com>
2018-05-04 17:06:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  var c = a+b;
+  return c;
+}
+
+function subtract(a, b) {
+  var c = a-b;
+  return c;
+}
+
+function multiply(a, b) {
+  var c = a*b;
+  return c;
+}
+
+function divide(a, b) {
+  var d = a/b;
+  return d;
+}
+
+function inc(n) {
+  var c = n+1;
+  return c;
+}
+
+function dec(n) {
+  var c = n-1;
+  return c;
+}
+
+function makeInt(n) {
+  var c = parseFloat('n');
+  return c;
+}
